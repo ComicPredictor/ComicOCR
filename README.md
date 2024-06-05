7b40c2699da158da67f17e5c5b7b4307518f1dfc
AnthonyYeh <scientistanthony@gmail.com>
2024-06-05 08:51:50 +0800

    hhh

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # ComicOCR
+please remember to go install tesseract.exe
+```bash
+pip install -r requirements.txt
+```
 ```python
 >>import usecomicsocr
 >>usecomicsocr.textnpos(image_path="path/to/image", show=True, log=True)
--- usecomicsocr.py
+++ usecomicsocr.py
@@ -2,8 +2,7 @@ from comicsocr import  api
 import logging
 import cv2
 def textnpos(image_path:str, show:bool=False, log:bool=False):
-    if not log:
-        logging.getLogger('comicsocr').propagate=False
+    logging.getLogger('comicsocr').propagate=log
     img = cv2.imread(image_path, cv2.IMREAD_COLOR) 
     #h_img, w_img, _ = img.shape
 
@@ -11,19 +10,18 @@ def textnpos(image_path:str, show:bool=False, log:bool=False):
     texes=[]
     for i, bbox in data:
         s=''
-        for j in i.split('\n'):
-            b=0
+        b=0
+        for j in i.split('\n'): 
             if j:
                 char, x, y, w, h, _ = j.split(" ")
                 x, y, w, h = int(x), int(y), int(w), int(h)
                 if char and char.lower() in " abcdefghijklmnopqrstuv,.?!" and x!= 0 and y != 0:
                     if b==0 and show:
                         cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 2)
-                    b=1
+                        b=1
                     s+=char
         if s:
             texes.append((s, bbox))
-        
     if show:
         cv2.imshow("iomg", img)
         cv2.waitKey(0)  
